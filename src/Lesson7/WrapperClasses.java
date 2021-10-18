08aaf156edb3d476f7ba7ee041878faa4a8ceb05
ZannaJ <zhanna.jelatanceva@gmail.com>
2021-10-18 21:39:05 +0300

    changes

---
+++ src/Lesson7/WrapperClasses.java
@@ -0,0 +1,17 @@
+package Lesson7;
+
+public class WrapperClasses {
+
+
+
+    public static void main(String[] args) {
+        int simpleInt = 5;
+
+        Integer.toString(simpleInt);
+
+    }
+    private int findMinimum(int a , int b ){
+        return Integer.min(a,b);
+    }
+
+}
